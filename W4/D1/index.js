9e5b54dbcb984267693a1c64bae5bf61eec60c15
saumya2611 <144019977+saumya2611@users.noreply.github.com>
2023-11-16 23:21:05 +0530

    Add files via upload

---
+++ W4/D1/index.js
@@ -0,0 +1,21 @@
+console.log("This is index.js")
+var name="gaurav";
+var age=15;
+console.log("name is"+name);
+console.log("Age is", age);
+
+// Array
+var marks=[67,89,12,34,56,78,21];
+console.log(marks);
+console.table(marks);
+
+// object
+var student={
+    "name":"hemant",
+    "age":20,
+    "marks":30,
+    "city":"delhi"
+}
+
+console.log(student);
+console.table(student);
