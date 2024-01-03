19533c15f3b1e513fa7f8ffb649e5a75545ce9fb
saumya2611 <144019977+saumya2611@users.noreply.github.com>
2024-01-03 22:26:46 +0530

    Add files via upload

---
+++ M1/W1/D2/02variables/script.js
@@ -0,0 +1,68 @@
+// Ways to declare a variable
+// var , let, const
+
+let firstName = 'John';
+let lastName = 'Doe';
+
+console.log(firstName, lastName );
+
+var age = 30;
+
+console.log(age);
+
+// Naming Conventions
+
+// - Only  letters, numbers, underscores and other dollar signs
+// - Can't start with a number
+
+// Multiple-Word Formatting 
+// firstName  = camelCase
+// first_Name = underscore
+// FirstName = PascalCase
+// firstname = Lowercase
+
+// Re-assigngning Variables
+age = 31;
+
+console.log(age);
+
+let score;
+
+score = 1;
+
+console.log(score);
+
+if(true) {
+    score = score + 1;
+}
+
+console.log(score);
+
+const x = 100;
+
+const arr = [1,2,3,4];
+
+console.log(arr)
+
+arr.push(6);
+
+console.log(arr);
+
+const person = {
+    name: 'Brad'
+};
+
+person.name = 'john';
+
+person.email = 'somu0438@gmail.com';
+
+console.log(person);
+
+// Declare multiple values at once
+let a, b, c;
+
+const d = 10, e = 20, f = 30;
+
+console.log(d);
+
+console.log(a);
