c79d8b412747ae9ab4202a89bfc4f877fa86d856
saumya2611 <144019977+saumya2611@users.noreply.github.com>
2024-01-04 23:36:15 +0530

    Add files via upload

---
+++ M1/W1/D3/05-Type-Conversion/script.js
@@ -0,0 +1,32 @@
+// let amount = '100';
+
+// Convert string to number
+// amount = parseInt(amount);
+// amount = this is unary operator +amount;
+// amount = Number(amount);
+
+// console.log(amount, typeof amount);
+
+// Convert Number to String
+
+// let amount = 100;
+// amount = amount.toString();
+// amount = String(amount);
+//console.log(amount, typeof amount);
+
+
+// Convert string to decimal 
+/* let amount = '99.9';
+amount = parseFloat(amount);
+console.log(amount, typeof amount); */
+
+// Convert number to boolean
+
+/*let amount = 1;
+amount = Boolean(amount);
+console.log(amount, typeof amount); */
+
+// if will change string to number then will return nan-(not a number) string must always be number not character
+/* let amount = 'saumya';
+amount = Number(amount);
+console.log(amount, typeof amount); */
