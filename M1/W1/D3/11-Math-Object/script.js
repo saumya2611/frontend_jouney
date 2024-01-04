8ee2a4bc37d335a62f12378470464ab8cafc13e1
saumya2611 <144019977+saumya2611@users.noreply.github.com>
2024-01-04 23:37:49 +0530

    Add files via upload

---
+++ M1/W1/D3/11-Math-Object/script.js
@@ -0,0 +1,23 @@
+//  console.log(Math);
+
+x = Math.sqrt(9);
+
+x = Math.abs(-5);
+
+x = Math.round(4.4);
+
+x = Math.ceil(4.2);
+
+x = Math.floor(4.2);
+
+x = Math.pow(3,2);
+
+x = Math.min(1,2,3);
+
+x = Math.max(1,2,3);
+
+x = Math.random(); // it's will return random number
+
+ x = Math.floor(Math.random() * 100 + 1);
+
+console.log(x);
