7f89fba8aed4fc60fa5e97ecb9f5bd94533f013b
saumya2611 <144019977+saumya2611@users.noreply.github.com>
2024-01-05 23:34:36 +0530

    Add files via upload

---
+++ M1/W1/D4/06-Operators/script.js
@@ -0,0 +1,56 @@
+// 1. Arithmetic Operators
+
+let x;
+
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 5 % 5;
+
+// Concatenation 
+x = 'Hello'+ ' ' + 'World';
+
+
+// Exponent
+x = 2 ** 3;
+
+// Increment
+x = 1;
+x = x + 1; 
+x++;
+
+
+// Decrement
+x = 2;
+// x = x - 1
+x--;
+
+// 2. Assignment Operators
+x = 10;
+
+x+= 5;
+x-= 5;
+x*= 5;
+x/= 5;
+x%= 5;
+x**= 5;
+
+// 3. Comparison Operators
+
+x = 2 == 2  // Double equal check only value 
+
+x = 2 === '2' // triple equal check data and type
+
+
+x = 2 !== '2';
+
+x = 10 > 5;
+
+x = 10 < 5;
+
+x = 10 >= 5;
+
+x = 10 <= 5;
+
+// console.log(x);
