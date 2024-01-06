6756ad259e60562cece04d14ac54003e2e4254d5
saumya2611 <144019977+saumya2611@users.noreply.github.com>
2024-01-06 20:01:51 +0530

    Add files via upload

---
+++ M1/W1/D5/10-Numbers/script.js
@@ -0,0 +1,31 @@
+let x;
+
+// const num = new Number (5);
+
+// const num = new Number (500);
+
+const num = new Number (5);
+
+// console.log(typeof num);
+
+x = num.toString();
+
+x = num.toString().length;
+
+x = num.toFixed(2)
+
+x = num.toPrecision(3)
+
+x = num.toExponential(2); // doubt
+
+x = num.toLocaleString('en-US') // this return 5
+
+x = num.valueOf()
+
+x = Number.MAX_VALUE;
+x = Number.MIN_VALUE;
+
+// console.log(typeof x);
+
+console.log(x);
+
