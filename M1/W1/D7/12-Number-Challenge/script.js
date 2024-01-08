88c45bf0784540707500324f2694d01ebab2b5fe
saumya2611 <144019977+saumya2611@users.noreply.github.com>
2024-01-08 21:18:44 +0530

    Add files via upload

---
+++ M1/W1/D7/12-Number-Challenge/script.js
@@ -0,0 +1,31 @@
+const x = Math.floor(Math.random() * 100 + 1);
+
+const y = Math.floor(Math.random() * 50 + 1);
+
+
+// Get the sum
+const sum = x + y;
+const sumOutput = `${x} + ${y} = ${sum}`;
+console.log(sumOutput);
+
+// console.log(x , y , sum);
+
+// Get the diff
+const diff = x - y;
+const diffOutput = `${x} - ${y} = ${diff}`;
+console.log(diffOutput);
+
+// Get the product
+const pro = x * y;
+const proOutput = `${x} * ${y} = ${pro}`;
+console.log(proOutput);
+
+// Get the quotient
+const quot = x / y;
+const quotOutput = `${x} / ${y} = ${quot}`;
+console.log(quotOutput);
+
+// Get the remainder
+const rem = x % y;
+const remOutput = `${x} % ${y} = ${rem}`;
+// console.log(remOutput);
