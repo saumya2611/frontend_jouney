f5ce8f006f9effff773d559a484e1a02deba2646
saumya2611 <144019977+saumya2611@users.noreply.github.com>
2024-01-11 22:40:18 +0530

    Add files via upload

---
+++ M1/W2/D3/14-DateMethod&DateTimeFormat/script.js
@@ -0,0 +1,18 @@
+let x;
+let d = new Date('1/2/2020');
+
+x = d.toString();
+
+x = d.getTime();
+x = d.valueOf();
+
+x = d.getFullYear();
+
+x = d.getMonth();
+x = d.getMonth() + 1;
+
+x = d.getDate();
+
+x = d.getDay();
+
+console.log(x);
