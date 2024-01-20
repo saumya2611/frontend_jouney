fe1d01df9d99db2f5ab3b1ea5864172a1b71439e
saumya2611 <144019977+saumya2611@users.noreply.github.com>
2024-01-20 23:43:05 +0530

    Add files via upload

---
+++ M1/W3/D5/01Functions-Basics/style.js
@@ -0,0 +1,24 @@
+/* function sayHello() {
+    console.log('Hello world');
+}
+sayHello();
+
+function even(a) {
+    if(a % 2 ==0) {
+        console.log('even')
+    } else {
+        console.log('odd')
+    }
+}
+even(10)
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+add(2,3) */
+
+function substract(num1, num2) {
+    return num1 - num2 // return keyword return new variable
+}
+const result = substract(20, 5);
+console.log(result);
