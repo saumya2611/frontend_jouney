9eb42e9ff0d0060e972d9dce04d47d56734dee72
saumya2611 <144019977+saumya2611@users.noreply.github.com>
2024-01-31 23:15:38 +0530

    Add files via upload

---
+++ M1/W5/D2/10-Execution-Context/script.js
@@ -0,0 +1,4 @@
+// When you run any javascript, a special environment is created to handle the transformation & execution of code.
+//  This is called execution context.
+// There is global execution context as well as function execution context for every function invoked
+
