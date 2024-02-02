5789191ecffa09f3d7e0c2590c667b2e4e78ae53
saumya2611 <144019977+saumya2611@users.noreply.github.com>
2024-02-02 23:27:09 +0530

    Add files via upload

---
+++ M1/W5/D4/1-if-statements/script.js
@@ -0,0 +1,31 @@
+if (true) {
+    console.log('This is true');
+} 
+
+if (false) {
+    console.log('This is Not true');
+}
+
+const x = 10;
+const y = 15;
+
+if(x <= y) {
+    console.log(`${x} is graeter than or qual to ${y}`);
+}
+
+if(x === y) {
+    console.log(`${x} is equal to ${y}`);
+} else {
+    console.log(`${x} is not equal to ${y}`);
+}
+
+if (x !== y) {
+    var z =20;
+    console.log(`${z} is 20`);
+}
+
+console.log(z)
+
+// Shorthand If
+if(x >= y) console.log(`${x} is graeter than or qual to ${y}`);
+    else console.log('This is false');
