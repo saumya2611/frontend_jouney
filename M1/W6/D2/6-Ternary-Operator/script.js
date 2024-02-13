59aeffc057a3d2f4a1e11ca38cdd9736641963f5
saumya2611 <144019977+saumya2611@users.noreply.github.com>
2024-02-13 22:05:13 +0530

    Add files via upload

---
+++ M1/W6/D2/6-Ternary-Operator/script.js
@@ -0,0 +1,33 @@
+const age = 20;
+
+// Using an if statement
+if (age >= 18) {
+    console.log('You can vote');
+} else {
+    console.log(`You cannot vote`);
+}
+
+// using Ternary Operator
+age >= 18 ? console.log(`you can vote`) : console.log(`you cannot vote`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
