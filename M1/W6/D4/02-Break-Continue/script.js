08e661d22883588dee379be8035457aa42a4ceb7
saumya2611 <144019977+saumya2611@users.noreply.github.com>
2024-02-15 23:42:50 +0530

    Add files via upload

---
+++ M1/W6/D4/02-Break-Continue/script.js
@@ -0,0 +1,20 @@
+// Break
+
+for (let i = 0; i <= 10; i++) {
+    if (i === 6) {
+        console.log('Breaking....');
+        break;
+    }
+    console.log(i);
+}
+
+for (let i = 0; i <= 10; i++) {
+    if (i === 5) {
+        console.log('Skiping....');
+        continue;
+    }
+    console.log(i);
+}
+
+
+
