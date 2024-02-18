6bcd0707970198bb6014f4bca50790fe31178312
saumya2611 <144019977+saumya2611@users.noreply.github.com>
2024-02-18 23:29:31 +0530

    Add files via upload

---
+++ M1/W6/D7/01-Dom/script.js
@@ -0,0 +1,15 @@
+// console.dir(window.document);
+
+// console.dir(document.body.innerHTML);
+
+// console.log(document.links[0]);
+
+// document.body.innerHTML = '<h1>Hello World</h1>';
+
+// document.write('Hello from JS');
+
+console.log(document.getElementById('main'));
+const main = document.getElementById('main');
+// main.innerHTML = '<h1>Hello From Main </h1>';
+
+document.querySelector('#main a').innerText = 'Hello';
