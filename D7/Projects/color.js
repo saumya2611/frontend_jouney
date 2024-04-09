79dfdcefb15776e93f4f3beaed5a9eb8422f51c8
saumya2611 <144019977+saumya2611@users.noreply.github.com>
2024-04-09 23:23:36 +0530

    Add files via upload

---
+++ D7/Projects/color.js
@@ -0,0 +1,20 @@
+const button = document.querySelector('button');
+const body = document.body;
+const currentColor = document.querySelector('.current-color');
+
+
+function randomColorGenerator() {
+    const red = Math.floor(Math.random()* 256);
+    const green = Math.floor(Math.random()* 256);
+    const blue = Math.floor(Math.random()* 256);
+    const randomColor = `rgb(${red},${green},${blue})`
+    return randomColor
+}
+
+
+
+button.addEventListener('click',function() {
+    const randomColor =  randomColorGenerator();
+    body.style.background = randomColor;
+    currentColor.textContent = randomColor;
+})
