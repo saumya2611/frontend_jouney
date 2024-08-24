6a30b05adb211e4d0be8025f43d00ef14507309b
saumya2611 <144019977+saumya2611@users.noreply.github.com>
2024-08-24 23:29:25 +0530

    Add files via upload

---
+++ 1-Array-Basic/script.js
@@ -0,0 +1,27 @@
+let x;
+
+
+// Array Literal
+const numbers = [12, 45, 33, 39];
+
+
+// Array Constructor
+const fruits = new Array('apple', 'grape', 'orange');
+
+x = numbers[0]
+
+x = numbers[0] + numbers[3];
+
+x = `My favorite fruit is ${fruits[2]}`;
+
+x = numbers.length;
+
+fruits[2] = 'pear'
+
+// fruits.length = 2;
+
+fruits[3] = 'strawberry';
+
+x = fruits;
+
+console.log(x);
