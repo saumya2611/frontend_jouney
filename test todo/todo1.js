3ab1d057d5a625783ca264fb66fcf2c5b7f561d8
saumya2611 <144019977+saumya2611@users.noreply.github.com>
2024-09-19 22:58:37 +0530

    Add files via upload

--- test todo/todo1.js
+++ test todo/todo1.js
@@ -10,69 +10,69 @@ const body = document.body
 let todoArr = []
 
 
-submitBtn.addEventListener('click',function(){
-    const inputValue = inputText.value
-    if(!inputValue.trim()) {
-        alert('plzz fill input field')
-        return
-    }
-// console.log(inputValue)
+submitBtn.addEventListener('click', function () {
+  const inputValue = inputText.value
+  if (!inputValue.trim()) {
+    alert('plzz fill input field')
+    return
+  }
+  // console.log(inputValue)
 
-todoListItem(inputValue)
-todoArr.push(inputValue)
-localStorage.setItem('data',JSON.stringify(todoArr))
-inputText.value = ''
+  todoListItem(inputValue)
+  todoArr.push(inputValue)
+  localStorage.setItem('data', JSON.stringify(todoArr))
+  inputText.value = ''
 })
 
 function todoListItem(item) {
-    const li = document.createElement('li')
-    li.innerHTML = `<div>
+  const li = document.createElement('li')
+  li.innerHTML = `<div>
     <span id="text">${item}</span>
   </div>
   <div class="button">
     <button class="add" title="Mark To Done">&#10003;</button>
     <button class="remove" title="Remove Item">&#10007;</button>
   </div>`
-  li.children[1].addEventListener('click',addRemoveItem)
-  console.log('saumya===',li.children[1])
+  li.children[1].addEventListener('click', addRemoveItem)
+  console.log('saumya===', li.children[1])
   todoList.append(li)
-} 
+}
 
-window.addEventListener('load',function(){
-   const data = JSON.parse(localStorage.getItem('data')) || []
-   todoArr = data
-   data.map((item)=>{
-    console.log('item is =>',item)
+window.addEventListener('load', function () {
+  const data = JSON.parse(localStorage.getItem('data')) || []
+  todoArr = data
+  data.map((item) => {
+    console.log('item is =>', item)
     todoListItem(item)
-   })
+  })
 
 })
 
 
-  function addRemoveItem(event) {
-  if(event.target.classList.contains('add')) {
+function addRemoveItem(event) {
+  if (event.target.classList.contains('add')) {
     const strikeItem = event.target.parentNode.parentNode.firstElementChild.firstElementChild;
-    console.log('addItem=========>',strikeItem)
+    console.log('addItem=========>', strikeItem)
     strikeItem.style.textDecoration = 'underline';
 
   }
 
-  if(event.target.classList.contains('remove')) {
-    
+  if (event.target.classList.contains('remove')) {
+
     const removeItem = event.target.parentNode.parentNode
-    console.log("removeItem====",removeItem)
+    console.log("removeItem====", removeItem)
     const clickItem = event.target.parentNode.parentNode.firstElementChild.firstElementChild.innerHTML
-    console.log('clickItem=========>',clickItem)
-   let dataArr = JSON.parse(localStorage.getItem('data'))
-   console.log('dataArr===',dataArr)
-   let selectItem = dataArr.filter((item)=>{
-    return item != clickItem
-   })
-   console.log('selectItem===',selectItem)
-   localStorage.setItem('data',JSON.stringify(selectItem))
+    console.log('clickItem=========>', clickItem)
+    let dataArr = JSON.parse(localStorage.getItem('data'))
+    console.log('dataArr===', dataArr)
+    let selectItem = dataArr.filter((item) => {
+      return item != clickItem
+    })
+    console.log('selectItem===', selectItem)
+    localStorage.setItem('data', JSON.stringify(selectItem))
 
-   removeItem.remove()
+    removeItem.remove()
   }
-} 
+}
 
-addRemoveButton.addEventListener('click',addRemoveItem)
+addRemoveButton.addEventListener('click', addRemoveItem)
